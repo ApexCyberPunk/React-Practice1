4937a6b6aa56424068bd786f1a0dfd40d4fc3d3e
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-23 02:29:24 -0700

    ok
    starting to understand react... yayy!

--- pract/src/index.js
+++ pract/src/index.js
@@ -2,33 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+
+const firstBook = {
+    author: 'bookie moor',
+    title: 'heaven Sent',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/61sp+sMfrXL._AC_UL600_SR600,400_.jpg'
+}
+
+const secondBook = {
+    author: 'mike boo',
+    title: 'get it done',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51XwpaLa1oL._AC_UL600_SR600,400_.jpg'
+}
+
 function BookList() {
     return (
         <section className='booklist'>
-        <Book job='devops'/>
-        <Book title='random title' number={22}/>
-       
+        <Book author={firstBook.author} title={firstBook.title} img={firstBook.img}/>
+        <Book author={secondBook.author} title={secondBook.title} img={secondBook.img}/>
         </section>
         
     )
 }
 
-const someFunc = (param1, param2) => {
-    console.log(param1, param2)
-}
-
-someFunc('job', 'developer')
 
 const Book = (props) => {
 
-    const title = 'Dems hate america';
     return (
         <article className='book'>
-        <img src={img} alt={title}/>
-      <h2>{title}</h2>
-      <p>{props.job}</p>
-      <p>{props.title}</p>
-      <p>{props.number}</p>
+        <img src={props.img} alt={props.title}/>
+      <h2>{props.title}</h2>
+      <p>{props.author}</p>
+      
     
     {console.log(props)}
         </article>
@@ -36,12 +41,6 @@ const Book = (props) => {
 }
 
 
-
-const img = <img alt='The Democrat Party Hates America' src='https://images-na.ssl-images-amazon.com/images/I/61sp+sMfrXL._AC_UL600_SR600,400_.jpg'/>
-const Title = () => <h1>The Democrat Party Hates America</h1>
-const Author = () => {
-    return <p style={{color: 'white', fontSize: '1em', background: 'blue'}}>Mark R. Levin</p>
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
