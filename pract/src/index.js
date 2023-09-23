87971bc8aa6d1714b774400cb967ac02da207ebd
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-22 22:31:57 -0700

    update

--- pract/src/index.js
+++ pract/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-export default function Greeting() {
-    return <h2>heyyy</h2>
-}
 
+function Greeting() {
+    return (
+        <div>
+            <h2>My first compoennt
+            <Person/>
+            <Message/>
+            </h2>
+        </div>
+    )
+}
+// function Greeting() {
+//     return React.createElement('h2', {}, 'hello world')
+// }
+
+const Person = () => <p>Jonny</p>
+const Message = () => {
+    return <p>whooo bro !!</p>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
